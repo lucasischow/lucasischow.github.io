2335cccc89cfbc140b022cdbeeefee8328ab222f
yahone <yahone.chow@gmail.com>
2026-04-05 20:59:40 +0900

    ###

--- data/timeline/index.ts
+++ data/timeline/index.ts
@@ -10,7 +10,7 @@ const data: TimelineNode[] = [
           openInNewTab: true,
           linkText: 'Site',
           link: 'https://github.com/17x/venus',
-          stacks: 'React 19, TypeScript, Vite, Tailwind CSS 4, Web Worker, SharedArrayBuffer, CanvasKit (Skia/WASM), FlatBuffers (schema), pnpm Workspace Monorepo, ESLint'
+          stacks: 'React 19, TypeScript, Vite, Tailwind CSS 4, Web Worker, SharedArrayBuffer, Canvas, CanvasKit (Skia/WASM), FlatBuffers (schema), pnpm Workspace Monorepo, ESLint'
         }
       }
     ]
